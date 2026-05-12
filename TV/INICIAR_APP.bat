d62e0cb6f457b78b2994fb67a703e0dcce652155
jucielidelima-sys <jucielidelima@gmail.com>
2026-05-12 09:35:51 -0300

    Delete TV directory

--- TV/INICIAR_APP.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d %~dp0
-echo Instalando/validando Streamlit...
-python -m pip install -r requirements.txt
-echo Abrindo painel em modo TV...
-python -m streamlit run app.py --server.address localhost --server.port 8501 --browser.gatherUsageStats false
-pause
